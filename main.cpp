a3bb3c1b363706cb090ea43296013d7200e305f5
AveeMaria <gasper.muhvic@protonmail.com>
2025-03-09 11:24:39 +0100

    idk

--- main.cpp
+++ main.cpp
@@ -2,45 +2,20 @@
 #include "include/Utils.hpp"
 #include "include/PacketType.hpp"
 
-SDL_Renderer* renderer = nullptr;
-
-void draw(SDL_Rect* r) {
-	SDL_RenderClear(renderer);
-
-    SDL_SetRenderDrawColor(renderer, 255, 0, 0, 255);
-    SDL_RenderFillRect(renderer, r);
-	SDL_SetRenderDrawColor(renderer, 255, 255, 255, 255);
-
-    SDL_RenderPresent(renderer);
-}
 
 int main() {
-	SDL_Window* window;
-
+	std::cout << "CLIENTSIDE\n";
 	if (SDLNet_Init() < 0) {
 		std::cerr << "ERROR: SDLNet_Init failed: " << SDLNet_GetError() << "\n";
 		SDL_Quit();
 	}
-
-	if (SDL_Init(SDL_INIT_VIDEO | SDL_INIT_EVENTS | SDL_INIT_TIMER) == 0)
-	{
-		window = SDL_CreateWindow("xd", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 1000, 600, NULL);
-		renderer = SDL_CreateRenderer(window, -1, 0);
-		if (renderer) {
-			SDL_SetRenderDrawColor(renderer, 255, 255, 255, 255);
-
-			//simuliramo slow load time
-			std::this_thread::sleep_for(std::chrono::milliseconds(300));
-		}
-	}
-
 	
 	Comms comms("127.0.0.1", (Uint16)123456);
 	//Comms comms("84.20.249.112", (Uint16)12345);
 	//Comms comms("192.168.0.24", (Uint16)12345);
 
 
-	if (comms.stack_send(SYN { SDL_GetTicks()})) {
+	if (comms.stack_send( SYN { SDL_GetTicks()} )) {
 		std::cout << "SYN SENT\n";
 	}
 	
@@ -78,7 +53,7 @@ int main() {
 			case (int)PacketType::ACK:
 				std::cout << "ERROR: type: ACK\n";//TEGA CLIENT NE SPREJEMA KER POSLJE
 				break;
-				///////
+				//////
 			case 40:
 				std::cout << "type: ENTITY_POS\n";
 				break;
